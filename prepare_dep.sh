70efc8fe3bdedc344eaf2402dc92c33af309ea53
Yaroslav Chirkov <aidekqz@gmail.com>
2024-06-03 14:15:31 +0300

    update prepare_dep.sh

--- prepare_dep.sh
+++ prepare_dep.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 set -ex
-sudo apt update && sudo apt install docker.io postgresql-client make gdb --yes
+sudo apt update && sudo DEBIAN_FRONTEND=noninteractive apt install docker.io postgresql-client make gdb --yes
 sudo usermod -aG docker $USER && echo "NOW RELOGIN"
