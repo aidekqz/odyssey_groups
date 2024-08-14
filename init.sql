26e01b32f67cc1cf9dea32da6aca58fde1318c08
Yaroslav Chirkov <aidekqz@gmail.com>
2024-08-14 18:16:43 +0300

    add conf for testing group with trust

--- init.sql
+++ init.sql
@@ -1,8 +1,11 @@
 ALTER SYSTEM SET password_encryption = 'md5';
 SELECT pg_reload_conf();
 SET password_encryption = 'md5';
-ALTER ROLE postgres PASSWORD '123456';
+
+ALTER ROLE postgres PASSWORD 'postgres123';
+CREATE ROLE group_checker WITH LOGIN PASSWORD 'group_checker123' SUPERUSER;
+CREATE ROLE auth_query_user WITH LOGIN PASSWORD 'auth_query_user123' SUPERUSER;
 CREATE DATABASE db1;
-CREATE ROLE user1 WITH LOGIN PASSWORD '111111';
+CREATE ROLE user1 WITH LOGIN PASSWORD 'user1123';
 CREATE ROLE group1;
 GRANT group1 TO user1;
--- test_group.sh
+++ test_group.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 
-psql "host=127.0.0.1 port=6432 user=user1 password=111111 dbname=db1" -c "SELECT 1 AS res" || {
+psql "host=127.0.0.1 port=6432 user=user1 password=user1123 dbname=db1" -c "SELECT current_user AS res" || {
 	echo "ERROR: expect good connection"
 	exit 1
 }
