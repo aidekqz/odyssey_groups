e700f3dadddffcd3b7c280aac9bc48960b9d126a
Yaroslav Chirkov <aidekqz@gmail.com>
2024-08-16 21:42:53 +0300

    add rsync.sh

---
+++ rsync.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+DST_IP=""
+PUB_KEY=""
+if [ "${DST_IP}" == "" ]; then
+    echo "define DST_IP var"
+    exit 1
+fi
+rsync -apv -e "ssh -i ${PUB_KEY}" --exclude ".git" ./ ${DST_IP}:~/odyssey_group --delete
