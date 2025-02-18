f6af7a39f930ae6298c742dd7e496a01d9fc6594
Iaroslav Chirkov <Iaroslav Chirkov>
2025-02-18 15:47:14 +0300

    update rsync.sh

--- scripts/rsync.sh
+++ scripts/rsync.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 DST_IP=""
-PUB_KEY=""
-# ODYSSEY_REPO should end by "/"
-# ODYSSEY_REPO=""
+PRIV_KEY=""
+DST_DIR="~/odyssey/group"
 
 if [ "${DST_IP}" == "" ]; then
     echo "define DST_IP var"
     exit 1
 fi
 
-if [ "${DST_IP}" == "" ]; then
-    echo "define PUB_KEY var"
+if [ "${PRIV_KEY}" == "" ]; then
+    echo "define PRIV_KEY var"
     exit 1
 fi
-rsync -apv -e "ssh -i ${PUB_KEY}" --exclude ".git" ./ ${DST_IP}:~/odyssey_group --delete --delete-excluded
-# rsync -apv -e "ssh -i ${PUB_KEY}" ${ODYSSEY_REPO} ${DST_IP}:/tmp/odyssey --delete
+rsync -apv -e "ssh -i ${PRIV_KEY}" --exclude ".git" ./ ${DST_IP}:${DST_DIR} --delete --delete-excluded
