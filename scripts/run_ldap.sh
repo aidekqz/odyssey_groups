b6e2590e6fd1d63a3a250844186d1c4c2e4fca6e
Yaroslav Chirkov <Aidekqzcloud@yandex.ru>
2025-05-30 20:03:54 +0300

    update ldap

--- scripts/run_ldap.sh
+++ scripts/run_ldap.sh
@@ -3,11 +3,18 @@
 function run_ldap() {
     docker rm --force openldap 2>/dev/null || true
     docker run --name openldap \
+        --hostname ldap.example.local \
+        -e LDAP_TLS=true \
         -e LDAP_DOMAIN="example.local" \
         -e LDAP_ADMIN_PASSWORD="oD2quooDaimulaegei7w" \
         -e LDAP_CONFIG_PASSWORD="oD2quooDaimulaegei7w" \
         -p 127.0.0.1:389:389 \
+        -p 127.0.0.1:636:636 \
         -d osixia/openldap:1.5.0
+}
+
+function prepapre_certs() {
+    echo "TODO: write code for certs generating"
 }
 
 function copy_files() {
@@ -25,6 +32,7 @@ function my_sleep() {
     sleep $1
 }
 
+prepapre_certs
 run_ldap
 my_sleep 4
 copy_files
